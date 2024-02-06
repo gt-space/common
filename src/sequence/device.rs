bc21573ebf5b7e4c71751e6d18e685548ed0c922
jeffcshelton <jeffrey.shelton.c@gmail.com>
2024-02-05 22:51:13 -0500

    Provide good error handling in sequences library

--- src/sequence/device.rs
+++ src/sequence/device.rs
@@ -1,7 +1,8 @@
-use std::net::UdpSocket;
-
+use crate::comm::SamControlMessage;
+use jeflog::fail;
 use pyo3::{pyclass, pyclass::CompareOp, pymethods, types::PyNone, IntoPy, PyAny, PyObject, PyResult, Python, ToPyObject};
-use super::{SAM_SOCKET, VEHICLE_STATE};
+use std::net::{ToSocketAddrs, UdpSocket};
+use super::{MAPPINGS, SAM_SOCKET, VEHICLE_STATE};
 
 /// A Python-exposed class that allows for interacting with a sensor.
 #[pyclass]
@@ -21,14 +22,17 @@ impl Sensor {
 	/// Reads the latest sensor measurements by indexing into the global vehicle state.
 	pub fn read(&self) -> PyObject {
 		let mut measurement = None;
-		let vehicle_state = VEHICLE_STATE.get();
 
-		if let Some(vehicle_state) = vehicle_state {
+		if let Some(vehicle_state) = VEHICLE_STATE.get() {
 			if let Ok(vehicle_state) = vehicle_state.lock() {
 				measurement = vehicle_state.sensor_readings
 					.get(&self.name)
 					.cloned();
+			} else {
+				fail!("Failed to lock vehicle state in method common::sequence::Sensor::read.");
 			}
+		} else {
+			fail!("Sequences library has not been initialized. Call the initialize function before running a sequence.");
 		}
 
 		Python::with_gil(|py| {
@@ -44,6 +48,7 @@ impl Sensor {
 	}
 }
 
+/// A Python-exposed class that allows for interacting with a valve.
 #[pyclass]
 #[derive(Clone, Debug)]
 pub struct Valve {
@@ -52,6 +57,7 @@ pub struct Valve {
 
 #[pymethods]
 impl Valve {
+	/// Constructs a new `Valve` with its mapping's text ID.
 	#[new]
 	pub fn new(name: String) -> Self {
 		Valve { name }
@@ -59,12 +65,54 @@ impl Valve {
 
 	/// Instructs the SAM board to open the valve.
 	pub fn open(&self) {
-		let socket = SAM_SOCKET.get_or_init(|| UdpSocket::bind(("0.0.0.0", 0)).unwrap());
-		
+		self.actuate(true);
 	}
 
 	/// Instructs the SAM board to close the valve.
 	pub fn close(&self) {
+		self.actuate(false);
+	}
 
+	/// Instructs the SAM board to actuate a valve.
+	pub fn actuate(&self, open: bool) {
+		let Some(mappings) = MAPPINGS.get() else {
+			fail!("Sequences library has not been initialized. Call the initialize function before running a sequence.");
+			return;
+		};
+
+		let Ok(mappings) = mappings.lock() else {
+			fail!("Failed to lock mappings in method common::sequence::Valve::actuate.");
+			return;
+		};
+
+		// TODO: replace with tcp socket
+		let socket = SAM_SOCKET.get_or_init(|| UdpSocket::bind(("0.0.0.0", 0)).unwrap());
+
+		let Some(mapping) = mappings.iter().find(|mapping| mapping.text_id == self.name) else {
+			fail!("Failed to actuate valve: mapping {} is not defined.", self.name);
+			return;
+		};
+		
+		let message = SamControlMessage::ActuateValve { channel: mapping.channel, open };
+
+		// TODO: switch to tcp
+		let address = format!("{}.local:8378", mapping.board_id)
+			.to_socket_addrs()
+			.ok()
+			.and_then(|mut addresses| addresses.find(|address| address.is_ipv4()));
+
+		if let Some(address) = address {
+			let Ok(serialized) = postcard::to_allocvec(&message) else {
+				fail!("Failed to actuate valve: could not serialize message with Postcard.");
+				return;
+			};
+
+			if let Err(error) = socket.send_to(&serialized, address) {
+				fail!("Failed to actuate valve: {error}");
+				return;
+			}
+		} else {
+			fail!("Failed to actuate valve: address of board '{}' not found.", mapping.board_id);
+		}
 	}
 }
--- src/sequence/mod.rs
+++ src/sequence/mod.rs
@@ -1,18 +1,18 @@
-pub mod device;
-pub mod func;
-pub mod unit;
+mod device;
+mod func;
+mod unit;
 
 pub use device::*;
 pub use func::*;
-use jeflog::warn;
+use jeflog::{fail, warn};
 use pyo3::{pymodule, types::PyModule, wrap_pyfunction, Py, PyResult, Python};
 pub use unit::*;
 
-use crate::comm::{Sequence, VehicleState};
+use crate::comm::{NodeMapping, Sequence, VehicleState, ChannelType};
 use std::{net::UdpSocket, sync::{Arc, Mutex, OnceLock}};
 
 #[pymodule]
-pub fn sequences(py: Python<'_>, module: &PyModule) -> PyResult<()> {
+fn sequences(py: Python<'_>, module: &PyModule) -> PyResult<()> {
 	module.add_class::<Duration>()?;
 	module.add_class::<Pressure>()?;
 	module.add_class::<ElectricPotential>()?;
@@ -23,6 +23,7 @@ pub fn sequences(py: Python<'_>, module: &PyModule) -> PyResult<()> {
 	module.add("us", Py::new(py, Duration::new(0.000001))?)?;
 	module.add("psi", Py::new(py, Pressure::new(1.0))?)?;
 	module.add("V", Py::new(py, ElectricPotential::new(1.0))?)?;
+	module.add("mV", Py::new(py, ElectricPotential::new(0.001))?)?;
 	module.add("F", Py::new(py, Temperature::new(1.0))?)?;
 
 	module.add_class::<Sensor>()?;
@@ -34,20 +35,62 @@ pub fn sequences(py: Python<'_>, module: &PyModule) -> PyResult<()> {
 	Ok(())
 }
 
+// these are global, static variables defined for use in the sequences library and nowhere else.
+// they are necessary for passing this global state to be read or written to in the Valve and Sensor methods.
+// all of these should be properly and fully defined with initialize.
 pub(crate) static VEHICLE_STATE: OnceLock<Arc<Mutex<VehicleState>>> = OnceLock::new();
 pub(crate) static SAM_SOCKET: OnceLock<UdpSocket> = OnceLock::new();
+pub(crate) static MAPPINGS: OnceLock<Arc<Mutex<Vec<NodeMapping>>>> = OnceLock::new();
 
 /// Initializes the sequences portion of the library.
-pub fn initialize(vehicle_state: Arc<Mutex<VehicleState>>) {
-	if VEHICLE_STATE.set(vehicle_state).is_err() {
+pub fn initialize(vehicle_state: Arc<Mutex<VehicleState>>, mappings: Arc<Mutex<Vec<NodeMapping>>>) {
+	if VEHICLE_STATE.set(vehicle_state).is_err() || MAPPINGS.set(mappings).is_err() {
 		warn!("Sequences library has already been initialized. Ignoring reinitialization.");
 		return;
 	}
 
+	pyo3::append_to_inittab!(sequences);
 	pyo3::prepare_freethreaded_python();
 }
 
+// TODO: change the run function to return an error in the event of one instead of printing out the error
+
 /// Runs a sequence. The `initialize` function must be called before this.
 pub fn run(sequence: Sequence) {
+	let Some(mappings) = MAPPINGS.get() else {
+		fail!("Sequences library has not been initialized. Call the initialize function before running a sequence.");
+		return;
+	};
 
+	let Ok(mappings) = mappings.lock() else {
+		fail!("Mappings could not be locked within common::sequence::run.");
+		return;
+	};
+
+	Python::with_gil(|py| {
+		if let Err(error) = py.run("from sequences import *", None, None) {
+			fail!("Failed to import sequences library: {error}");
+			return;
+		}
+
+		for mapping in &*mappings {
+			// TODO: inspect this definition again. this may be redefining values unnecessarily.
+			let definition = match mapping.channel_type {
+				ChannelType::ValveVoltage | ChannelType::ValveCurrent => format!("{0} = Valve('{0}')", mapping.text_id),
+				_ => format!("{0} = Sensor('{0}')", mapping.text_id),
+			};
+
+			if let Err(error) = py.run(&definition, None, None) {
+				fail!("Failed to define {} as a mapping: {error}", mapping.text_id);
+				return;
+			}
+		}
+
+		// drop the lock before entering script to prevent deadlock
+		drop(mappings);
+
+		if let Err(error) = py.run(&sequence.script, None, None) {
+			fail!("Failed to run sequence '{}': {error}", sequence.name);
+		}
+	});
 }
