c4ab2aa3ad6e7834f7ba4bc0a506545312dfe5a3
jeffcshelton <jeffrey.shelton.c@gmail.com>
2024-03-05 23:01:07 -0500

    Add device handler closure separate

--- src/sequence/device.rs
+++ src/sequence/device.rs
@@ -1,8 +1,7 @@
-use crate::comm::SamControlMessage;
+use crate::comm::ValveState;
 use jeflog::fail;
 use pyo3::{pyclass, pyclass::CompareOp, pymethods, types::PyNone, IntoPy, PyAny, PyObject, PyResult, Python, ToPyObject};
-use std::net::{ToSocketAddrs, UdpSocket};
-use super::{MAPPINGS, SAM_SOCKET, VEHICLE_STATE};
+use super::{DeviceAction, DEVICE_HANDLER};
 
 /// A Python-exposed class that allows for interacting with a sensor.
 #[pyclass]
@@ -21,24 +20,17 @@ impl Sensor {
 
 	/// Reads the latest sensor measurements by indexing into the global vehicle state.
 	pub fn read(&self) -> PyObject {
-		let mut measurement = None;
+		let Some(device_handler) = &*DEVICE_HANDLER.lock().unwrap() else {
+			fail!("Device handler not set before accessing external device.");
+			return Python::with_gil(|py| PyNone::get(py).to_object(py));
+		};
 
-		if let Some(vehicle_state) = VEHICLE_STATE.get() {
-			if let Ok(vehicle_state) = vehicle_state.lock() {
-				measurement = vehicle_state.sensor_readings
-					.get(&self.name)
-					.cloned();
-			} else {
-				fail!("Failed to lock vehicle state in method common::sequence::Sensor::read.");
-			}
-		} else {
-			fail!("Sequences library has not been initialized. Call the initialize function before running a sequence.");
-		}
+		let measurement = device_handler(&self.name, DeviceAction::ReadSensor);
 
 		Python::with_gil(|py| {
 			measurement.map_or(
 				PyNone::get(py).to_object(py),
-				|measurement| measurement.into_py(py)
+				|measurement| measurement.into_py(py),
 			)
 		})
 	}
@@ -75,44 +67,12 @@ impl Valve {
 
 	/// Instructs the SAM board to actuate a valve.
 	pub fn actuate(&self, open: bool) {
-		let Some(mappings) = MAPPINGS.get() else {
-			fail!("Sequences library has not been initialized. Call the initialize function before running a sequence.");
+		let Some(device_handler) = &*DEVICE_HANDLER.lock().unwrap() else {
+			fail!("Device handler not set before accessing external device.");
 			return;
 		};
 
-		let Ok(mappings) = mappings.lock() else {
-			fail!("Failed to lock mappings in method common::sequence::Valve::actuate.");
-			return;
-		};
-
-		// TODO: replace with tcp socket
-		let socket = SAM_SOCKET.get_or_init(|| UdpSocket::bind(("0.0.0.0", 0)).unwrap());
-
-		let Some(mapping) = mappings.iter().find(|mapping| mapping.text_id == self.name) else {
-			fail!("Failed to actuate valve: mapping {} is not defined.", self.name);
-			return;
-		};
-		
-		let message = SamControlMessage::ActuateValve { channel: mapping.channel, open };
-
-		// TODO: switch to tcp
-		let address = format!("{}.local:8378", mapping.board_id)
-			.to_socket_addrs()
-			.ok()
-			.and_then(|mut addresses| addresses.find(|address| address.is_ipv4()));
-
-		if let Some(address) = address {
-			let Ok(serialized) = postcard::to_allocvec(&message) else {
-				fail!("Failed to actuate valve: could not serialize message with Postcard.");
-				return;
-			};
-
-			if let Err(error) = socket.send_to(&serialized, address) {
-				fail!("Failed to actuate valve: {error}");
-				return;
-			}
-		} else {
-			fail!("Failed to actuate valve: address of board '{}' not found.", mapping.board_id);
-		}
+		let state = if open { ValveState::Open } else { ValveState::Closed };
+		device_handler(&self.name, DeviceAction::ActuateValve { state });
 	}
 }
